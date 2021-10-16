06fa934b7b34ebbfd546ed6722471eef5a49240a
anilrai01 <anil.rai01041998@gmail.com>
2021-10-16 23:48:56 +0545

    Added user repo state context

--- src/context/UserDataContext.js
+++ src/context/UserDataContext.js
@@ -4,9 +4,12 @@ export const UserDataContext = createContext();
 
 export const UserDataContextProvider = (props) => {
   const [userData, setUserData] = useState(null);
+  const [userRepoData, setUserRepoData] = useState([]);
 
   return (
-    <UserDataContext.Provider value={{ userData, setUserData }}>
+    <UserDataContext.Provider
+      value={{ userData, setUserData, userRepoData, setUserRepoData }}
+    >
       {props.children}
     </UserDataContext.Provider>
   );
--- src/hooks/useGitHubApi.js
+++ src/hooks/useGitHubApi.js
@@ -5,8 +5,8 @@ import GhPolyglot from "gh-polyglot";
 
 export default function useGitHubApi() {
   const [loadingStat, setLoadingStat] = useState(false);
-  const { setUserData } = useUserDataContext();
-  const [userLangChart, setUserLangChart] = useState(null);
+  const { setUserData, setUserRepoData } = useUserDataContext();
+  const [userLangChart, setUserLangChart] = useState([]);
 
   const requestUserData = async (username, callback = false) => {
     setLoadingStat(true);
@@ -15,7 +15,7 @@ export default function useGitHubApi() {
       const response = await axios.get(
         `https://api.github.com/users/${username}`
       );
-      // console.log("Responses: ", response.data);
+      // console.log("User Data Responses: ", response.data);
       setLoadingStat(false);
       setUserData(response.data);
 
@@ -40,16 +40,30 @@ export default function useGitHubApi() {
 
     // User stats
     // https://api.github.com/users/anilrai01/autoShop/repos?per_page=100&page=1
-    if (username !== "") {
-      user.userStats(function (err, stats) {
-        if (err) {
-          console.log("Error: ", err);
-        } else {
-          // console.log("Repository Language Stat: ", stats);
-          setUserLangChart(stats);
-          // return stats;
-        }
-      });
+    user.userStats(function (err, stats) {
+      if (err) {
+        console.log("Error: ", err);
+      } else {
+        // console.log("Repository Language Stat: ", stats);
+        setUserLangChart(stats);
+        // return stats;
+      }
+    });
+  };
+
+  const requestUserRepository = async (username) => {
+    setLoadingStat(true);
+
+    try {
+      const response = await axios.get(
+        `https://api.github.com/users/${username}/repos`
+      );
+      // console.log("Repo Response: ", response.data);
+      setLoadingStat(false);
+      setUserRepoData(response.data);
+    } catch (error) {
+      setLoadingStat(false);
+      console.log("Error on requesting repo data: ", error);
     }
   };
 
@@ -58,5 +72,6 @@ export default function useGitHubApi() {
     requestUserData,
     userLangChart,
     requestUserPolyglotChart,
+    requestUserRepository,
   };
 }
