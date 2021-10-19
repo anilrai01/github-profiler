2f0a75f1297ff07334cc025f47a6b1077402c6ae
anilrai01 <anil.rai01041998@gmail.com>
2021-10-19 22:42:59 +0545

    Added not found page for edge case handling

---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import InitialScreenWrapper from "./InitialScreenWrapper";
+
+export default function NotFound() {
+  return (
+    <InitialScreenWrapper>
+      <p>Something went wrong ! Please try again later</p>
+    </InitialScreenWrapper>
+  );
+}
